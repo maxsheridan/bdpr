676efdec55f1415afe119b8dd6ad3e4b1c9b64cc
maxsheridan <max@maxsheridan.com>
2024-07-26 13:41:07 +0300

    Update main.css, header.js

--- js/header.js
+++ js/header.js
@@ -1,6 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
+    updateHeaderHeight();
+    window.addEventListener('resize', updateHeaderHeight);
+});
+
+function updateHeaderHeight() {
     var header = document.querySelector('header');
     var root = document.documentElement;
     var headerHeight = header.offsetHeight + 'px';
     root.style.setProperty('--header-height', headerHeight);
-});
+}
